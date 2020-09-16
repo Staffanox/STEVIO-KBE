1fd701fc023218d359b70eaf81266639515ba065
staffano <s0565913@htw-berlin.de>
2020-09-17 01:03:17 +0200

    fixed gateway for songlist

--- songMS/song-service/src/main/java/htwb/ai/steven/Controller/SongListController.java
+++ songMS/song-service/src/main/java/htwb/ai/steven/Controller/SongListController.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/songList")
+@RequestMapping("/songlist")
 public class SongListController {
 
     private RestTemplate restTemplate;
